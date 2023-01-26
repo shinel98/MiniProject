cd69ab5525424b25c96af29ff52da2ee73adbfc0
lukehongg <lukehongg@handong.ac.kr>
2023-01-26 22:04:26 +0900

    init

---
+++ src/main/java/com/greenpoint/server/exception/GlobalException.java
@@ -0,0 +1,18 @@
+package com.greenpoint.server.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public class GlobalException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+
+
+    public GlobalException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
