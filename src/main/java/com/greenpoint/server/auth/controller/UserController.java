3a61fb55b95dd5d5aecca33c2075e0ed45f10c59
shinel98 <75024473+shinel98@users.noreply.github.com>
2023-01-27 20:23:22 +0900

    feat[#13]
    회원가입 API, 회원탈퇴 API 구현

--- src/main/java/com/greenpoint/server/auth/controller/UserController.java
+++ src/main/java/com/greenpoint/server/auth/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.greenpoint.server.auth.controller;
 
+import com.greenpoint.server.auth.response.AccountResponse;
 import com.greenpoint.server.auth.response.LoginResponse;
 import com.greenpoint.server.auth.service.UserService;
 import com.greenpoint.server.customer.model.Customer;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -33,25 +35,39 @@ public class UserController {
         Customer userInfo = new Customer();
         obArr = userService.saveUser(code);
         System.out.println("obArr = " + obArr);
-        userInfo =(Customer)obArr[0];
-        isRegistered = (boolean)obArr[1];
+        userInfo = (Customer) obArr[0];
+        isRegistered = (boolean) obArr[1];
         LoginResponse loginResponse = new LoginResponse(userInfo.getKakaoToken(), userInfo.getNickname(), userInfo.getImage(), isRegistered);
         return ResponseEntity.ok(loginResponse);
     }
 
-    @GetMapping("/makeaccount")
-    public ResponseEntity<LoginResponse> getLogin(@RequestParam("code") String code) {
-
-        boolean isRegistered;
-        Object[] obArr = new Object[2];
-        Customer userInfo = new Customer();
-        obArr = userService.saveUser(code);
-        System.out.println("obArr = " + obArr);
-        userInfo =(Customer)obArr[0];
-        isRegistered = (boolean)obArr[1];
-        LoginResponse loginResponse = new LoginResponse(userInfo.getKakaoToken(), userInfo.getNickname(), userInfo.getImage(), isRegistered);
-        return ResponseEntity.ok(loginResponse);
+    @PostMapping("/makeAccount")
+    public ResponseEntity<AccountResponse> makeAccount(@RequestBody HashMap<String, String> param) {
+        Boolean result;
+        System.out.println("param = " + param);
+        result = userService.getUserInfoByForm(param);
+        if(result == false) {
+            AccountResponse accountResponse = new AccountResponse(false);
+            return ResponseEntity.ok(accountResponse);
+        }
+        AccountResponse accountResponse = new AccountResponse(true);
+        return ResponseEntity.ok(accountResponse);
     }
+
+    @DeleteMapping("/deleteMember")
+    public String deleteAccount(HttpSession session, @RequestParam("token") String token){
+        String resultCode = "";
+        int result = userService.memberDelete(token);
+        if(result == 0){
+            resultCode = "회원 탈퇴 실패";
+
+        } else {
+            session.removeAttribute("sessionId");
+            resultCode = "회원 탈퇴 성공";
+        }
+        return resultCode;
+    }
+
 }
 
 
--- src/main/java/com/greenpoint/server/customer/controller/CustomerController.java
+++ src/main/java/com/greenpoint/server/customer/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package com.greenpoint.server.customer.controller;
 
+import com.greenpoint.server.customer.model.Customer;
+import com.greenpoint.server.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class CustomerController {
 
-    @RequestMapping("/test")
-    public void print(){
-        System.out.println(" 들어옴! " );
+    @Autowired
+    private CustomerService cs;
+
+    @GetMapping("/test")
+    public void print() {
+        System.out.println(" 들어옴! ");
+
+        Customer customer;
+        customer = cs.findById(1L);
+        System.out.println("customer = " + customer.getNickname());
     }
 }
